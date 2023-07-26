09ee19a42f990661debda5866430e09754bfb20f
DROIDITO <140570587+DROIDITO@users.noreply.github.com>
2023-07-26 09:33:48 -0600

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,9 +4,9 @@ let group = m.chat
 conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-body: '⫷᭄ʜᴀᴅᴇs-ʙᴏᴛ-ᴍᴅ﹏✍',         
+body: '⫷᭄TONY-OFC﹏✍',         
 previewType: 0, thumbnail: fs.readFileSync("./views/Menu.jpg"),
-sourceUrl: `https://github.com/OFC-YOVANI/HADES-BOT-MD.git`}}})   
+sourceUrl: `https://chat.whatsapp.com/CkA0gnGz99l6fJgTbIvUBH`}}})   
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
